072ef72674279bb45806162fe332ded1e1a35abf
Dmitrii Ershov <dmitryershov0@gmail.com>
2020-07-26 10:39:44 +0300

    Initial project

---
+++ Main/IIndices.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+
+namespace Accuweather.Indices
+{
+	/// <summary>
+	/// Get Indices information for a specific location.
+	/// </summary>
+	public interface IIndices
+	{
+
+		/// <summary>
+		/// Returns 5 days of daily index data for a specific group of indices, by location key.
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="group">Index group ID.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <returns></returns>
+		Task<string> FiveDaysOfDailyIndexValuesByGroupIndex(int locationKey, int group, bool details = false);
+
+		/// <summary>
+		/// Returns 5 days of daily index data for a specific index, by location key.
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="specific">Specific Index ID.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <returns></returns>
+		Task<string> FiveDaysOfDailyIndexValuesBySpecificIndex(int locationKey, int specific, bool details = false);
+
+		/// <summary>
+		/// Returns 5 days of daily index data for all indices, by location key.
+		/// </summary>
+		/// <param name="locationKey">Unique ID to search for a specific location.</param>
+		/// <param name="details">Boolean value specifies whether or not to include full details in the response.</param>
+		/// <returns></returns>
+		Task<string> FiveDaysOfDailyIndexValues(int locationKey, bool details = false);
+	}
+}
---
+++ Main/Indices.cs
@@ -0,0 +1,62 @@
+﻿using Accuweather.Core.Helpers;
+using Accuweather.Core;
+using System.Threading.Tasks;
+
+namespace Accuweather.Indices
+{
+	/// <summary>
+	///	Accuweather Indices Api
+	/// </summary>
+	public class Indices : AccuweatherApiCore, IIndices
+	{
+		private const string _baseUrl = "http://dataservice.accuweather.com/indices/v1";
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="apiKey">Api key accuweather.</param>
+		/// <param name="language">Language.</param>
+		/// <returns></returns>
+		public Indices(string apiKey, string language = "en-us") : base(apiKey, language)
+		{
+
+		}
+		private object GetObjectIndices(bool details)
+		{
+			return new
+			{
+				language = _language,
+				details
+			};
+		}
+
+		///<inheritdoc cref="IIndices.FiveDaysOfDailyIndexValuesByGroupIndex(int, int, bool)"/>
+		public async Task<string> FiveDaysOfDailyIndexValuesByGroupIndex(int locationKey, int group,
+		bool details = false)
+		{
+			var obj = GetObjectIndices(details);
+			var url = $"{_baseUrl}/daily/5day/{locationKey}/groups/{group}?apikey={_apiKey}&";
+
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		///<inheritdoc cref="IIndices.FiveDaysOfDailyIndexValuesBySpecificIndex(int, int, bool)"/>
+		public async Task<string> FiveDaysOfDailyIndexValuesBySpecificIndex(int locationKey, int specific,
+		bool details = false)
+		{
+			var obj = GetObjectIndices(details);
+			var url = $"{_baseUrl}/daily/5day/{locationKey}/{specific}?apikey={_apiKey}&";
+
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+
+		///<inheritdoc cref="IIndices.FiveDaysOfDailyIndexValues(int, bool)"/>
+		public async Task<string> FiveDaysOfDailyIndexValues(int locationKey, bool details = false)
+		{
+			var obj = GetObjectIndices(details);
+			var url = $"{_baseUrl}/daily/5day/{locationKey}?apikey={_apiKey}&";
+
+			return await SendGetRequest(UrlEncodeHelper.UrlEncode(obj, url));
+		}
+	}
+}
